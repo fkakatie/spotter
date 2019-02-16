05b955f241fcf5bf61a5d9565df56f8ba736794d
fkakatie <sextonskatie@gmail.com>
2019-02-15 23:39:02 -0700

    updated db config

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,4 @@
+DROP DATABASE IF EXISTS workouts_db;
 CREATE DATABASE workouts_db;
 
 USE workouts_db;
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,11 +1,11 @@
 USE workouts_db;
 
 INSERT INTO workouts (workout) VALUES ('Squats');
-INSERT INTO workouts (workout) VALUES ('Superman Punch')
+INSERT INTO workouts (workout) VALUES ('Superman Punch');
 INSERT INTO workouts (workout) VALUES ('Push-ups');
 INSERT INTO workouts (workout) VALUES ('Single-leg Tricep Dip');
 INSERT INTO workouts (workout) VALUES ('Sit-ups');
-INSERT INTO workouts (workout) VALUES ('Air Swimming')
+INSERT INTO workouts (workout) VALUES ('Air Swimming');
 INSERT INTO workouts (workout) VALUES ('Split Squats');
 INSERT INTO workouts (workout) VALUES ('Breakdancer');
 INSERT INTO workouts (workout) VALUES ('Mountain Climbers');
